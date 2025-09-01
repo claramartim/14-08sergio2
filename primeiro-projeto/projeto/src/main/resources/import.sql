3582825ad677048fe0a45733897d9ce7110bf0e3
claramartim <271965@unifio.edu.br>
2025-09-01 23:30:07 +0000

    01/09/25

---
+++ primeiro-projeto/projeto/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+insert into Categoria (nome) values ('Bebida');
+insert into Categoria (nome) values ('Frutas');
+insert into Categoria (nome) values ('Carnes');
+insert into Categoria (nome) values ('Sorvetes');
+insert into Categoria (nome) values ('Verduras');
+insert into Categoria (nome) values ('Legumes');
+insert into Categoria (nome) values ('Bolacha');
+insert into Categoria (nome) values ('Carvão');
+insert into Categoria (nome) values ('Leite');
+insert into Categoria (nome) values ('Pães');
+insert into Fabricante (nome) values ('Alvorejo Bebidas Artesanais');
+
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Coca_Cola', 15, 12.00, '2026-12-10', 1, 1);
