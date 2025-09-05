102d77742ceb2f84ee3dc3791615586b4b789ec3
claramartim <271965@unifio.edu.br>
2025-09-05 01:29:16 +0000

    04/09/25

--- primeiro-projeto/projeto/src/main/resources/import.sql
+++ primeiro-projeto/projeto/src/main/resources/import.sql
@@ -8,6 +8,28 @@ insert into Categoria (nome) values ('Bolacha');
 insert into Categoria (nome) values ('Carvão');
 insert into Categoria (nome) values ('Leite');
 insert into Categoria (nome) values ('Pães');
+
 insert into Fabricante (nome) values ('Alvorejo Bebidas Artesanais');
+insert into Fabricante (nome) values ('Frutalize');
+insert into Fabricante (nome) values ('Carnera');
+insert into Fabricante (nome) values ('GelatoCraft');
+insert into Fabricante (nome) values ('VerdeViva');
+insert into Fabricante (nome) values ('Raízes & Sabores');
+insert into Fabricante (nome) values ('CrocaDelícia');
+insert into Fabricante (nome) values ('BrasaForte');
+insert into Fabricante (nome) values ('LeiteLuz');
+insert into Fabricante (nome) values ('Pão & Sabor');
 
 insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Coca_Cola', 15, 12.00, '2026-12-10', 1, 1);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Pera', 20, 5.00, '2026-03-17', 2, 2);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Picanha', 10, 100.00, '2026-06-20', 3, 3);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Picolé de Chocolate', 50, 3.50, '2026-09-04', 4, 4);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Alface', 60, 10.00, '2025-12-01', 5, 5);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Cenoura', 20, 6.00, '2025-10-07', 6, 6);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Bolacha Amanteigada', 200, 17.00, '2026-12-27', 7, 7);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Carvão para Churrasco', 150, 60.00, '2026-12-12', 8, 8);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Leite Semi Desnatado', 1000, 5.99, '2026-09-04', 9, 9);
+insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Pão Integal', 40, 17.00, '2026-10-19', 10, 10);
+
+
+
